94d31d5b93c9b5076741486cd65aa1d175861c84
Ayesha Ahmed <32429595+bladedancer-256@users.noreply.github.com>
2019-11-14 19:38:07 +1100

    Fixed spelling mistake

--- R/plotTelo.R
+++ R/plotTelo.R
@@ -11,12 +11,15 @@ plotTelo <- function(bed){
 
   bed <- read.table(bed)
   names(bed) = c("chrom_and_strand", "start", "end")
+  
   #step 2: separating the chromosome and strands into different columns
   bed = separate(bed, 1, into = c("chromosome", "strand"), sep="_", remove = TRUE) #using tidyverse's function
   bed
+  
   #step 3: replacing the r with "-"s for use as strand data
   bed$strand <- sub("r", "-", bed$strand)
   bed
+  
   #step 3: replacing the r with "+"s for use as strand data
   bed$strand <- sub("f", "+", bed$strand)
   bed
@@ -24,10 +27,12 @@ plotTelo <- function(bed){
   bed
   #step 4: plotting human chromosomes
   kp <- plotKaryotype()
+  
   #step 5: adding base numbers
   kpAddBaseNumbers(kp)
+  
   #step 6: plotting the rectangle indicators
-  #using chromosomoe, start and end coordinates asinput
+  #using chromosome, start and end coordinates as input
   kpRect(kp, chr = bed$chromosome, x0 = bed$start, x1= bed$end,
          y0 = 0.0, y1 =0.5, col = "yellow")
 }
